bcacbf11eb73481c671c02864537218c7efe7c33
Andon Zeno <tonizeno5@gmail.com>
2023-11-23 23:00:11 +0100

    Bug - Fix stripe api key retrieval

--- libs/stripe.ts
+++ libs/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+export const stripe = new Stripe(process.env.NEXT_SECRET_KEY as string, {
     apiVersion: "2023-10-16",
     appInfo: {
         name: "Stramify",
